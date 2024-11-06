189e3aa26581c9495330cdde59a99c9de4fc35d8
Omar Khater <omarkhater@tamu.edu>
2024-11-06 13:14:19 -0600

    Pass the unit tests for new table professional details

--- app/models/professional_detail.rb
+++ app/models/professional_detail.rb
@@ -1,3 +1,9 @@
 class ProfessionalDetail < ApplicationRecord
   belongs_to :profile
+
+  validates :company, presence: true, length: { maximum: 100 }
+  validates :industry, presence: true, length: { maximum: 50 }
+  validates :job_name, presence: true, length: { maximum: 50 }
+  validates :experience, length: { maximum: 50 }
+  validates :specialization, length: { maximum: 50 }
 end

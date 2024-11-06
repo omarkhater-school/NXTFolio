97ef929c80f03906d217fc82b2d47afaafb867fd
Omar Khater <omarkhater@tamu.edu>
2024-11-06 13:10:18 -0600

    Pass the unit tests for social link new table

--- app/models/social_link.rb
+++ app/models/social_link.rb
@@ -1,3 +1,5 @@
 class SocialLink < ApplicationRecord
   belongs_to :profile
+  validates :platform, presence: true, length: {maximum:50}
+  validates :url, presence: true, format: {with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL"}
 end

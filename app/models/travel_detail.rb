1d100b800c00e8590fad295a1f5fa7fea8cf02bc
Omar Khater <omarkhater@tamu.edu>
2024-11-06 13:18:52 -0600

    Pass the tests

--- app/models/travel_detail.rb
+++ app/models/travel_detail.rb
@@ -1,3 +1,18 @@
+# app/models/travel_detail.rb
 class TravelDetail < ApplicationRecord
   belongs_to :profile
+
+  validates :country, presence: true, length: { maximum: 50 }
+  validates :city, presence: true, length: { maximum: 50 }
+  validates :state, length: { maximum: 50 }
+  validates :details, length: { maximum: 255 }
+  validate :end_date_after_start_date
+
+  private
+
+  def end_date_after_start_date
+    if end_date.present? && start_date.present? && end_date < start_date
+      errors.add(:end_date, "must be after or on start date")
+    end
+  end
 end

b8eddd9de0d1fcff0a9e518e88cd64ff13ba201d
Yash Honrao <yashhonrao2024@gmail.com>
2024-11-12 13:07:44 -0600

    Step definitions for Password Requirement

--- features/step_definitions/sign_in_up_steps.rb
+++ features/step_definitions/sign_in_up_steps.rb
@@ -28,3 +28,65 @@ Then(/^I will be on (.+)$/) do |page_name|
   expected_path = path_to(page_name)
   expect(page).to have_current_path(expected_path, wait: 5)
 end
+
+# New step definitions for email authentication and password requirements
+
+Given("I have received an authentication email for {string}") do |email|
+  @authentication_email = email
+  # Implement logic to simulate receiving an authentication email
+  EmailService.send_authentication_email(email)
+end
+
+When("I click the authentication link in the email") do
+  # Simulate clicking the authentication link
+  visit authenticate_email_path(@authentication_email)
+end
+
+Then("an authentication email should be sent to {string}") do |email|
+  # Check if an authentication email was sent
+  expect(EmailService).to have_sent_email(to: email, subject: "Authenticate Your Account")
+end
+
+Given("I have signed up but not authenticated my email") do
+  @user = User.create(email: "unauthenticated@example.com", password: "ValidPass123!", email_authenticated: false)
+end
+
+When("I try to log in with my credentials") do
+  step "I fill in the following:", table(%{
+    | login_email    | unauthenticated@example.com |
+    | login_password | ValidPass123!               |
+  })
+  step 'I click on "SIGN IN"'
+end
+
+Then("I should see {string}") do |message|
+  expect(page).to have_content(message)
+end
+
+Then("I should not see any password-related error messages") do
+  expect(page).not_to have_content("Password must be at least 8 characters long")
+  expect(page).not_to have_content("Passwords do not match")
+  expect(page).not_to have_content("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+end
+
+Given("I am on the login page") do
+  visit login_path
+end
+
+When("I fill in the following:") do |table|
+  table.rows_hash.each do |field, value|
+    fill_in field, with: value
+  end
+end
+
+When("I click on {string}") do |button_text|
+  click_button button_text
+end
+
+Then("I should be on the new_general_info page") do
+  expect(current_path).to eq(new_general_info_path)
+end
+
+Then("I should be on the home page") do
+  expect(current_path).to eq(root_path)
+end
